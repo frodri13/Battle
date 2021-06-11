3a3973bedeaec99b027314414498b25ea2e8f545
Fabio <alessf13@gmail.com>
2021-06-11 10:09:12 +0100

    Form for entering names and view pages

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,9 @@
-feature 'Testing infrastructure' do
-  scenario 'can run app and check page content' do
+feature 'Enter names' do
+  scenario 'submitting names' do 
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in :player_1_name, with: 'Charlotte'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Charlotte vs. Mittens'
   end
 end
